252aaca31d4f77da3a1e614c2ff49533ab3b6508
Jo Bridts <jo.bridts@chiro.be>
2014-02-02 02:35:13 +0100

    added Button

--- OpBivak.ios/FirstViewController.designer.cs
+++ OpBivak.ios/FirstViewController.designer.cs
@@ -1,18 +1,28 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
 
 namespace OpBivak.ios
 {
 	[Register ("FirstViewController")]
 	partial class FirstViewController
 	{
+		[Outlet]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UIButton btnCallKipdorp { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnCallKipdorp != null) {
+				btnCallKipdorp.Dispose ();
+				btnCallKipdorp = null;
+			}
 		}
 	}
 }
-
