0a0ec657e5c76e0c277a111c8083e7b9229c1807
Jo Bridts <jo.bridts@chiro.be>
2014-02-02 17:55:07 +0100

    added in-app settings

--- OpBivak.ios/SettingsViewController.cs
+++ OpBivak.ios/SettingsViewController.cs
@@ -1,10 +1,89 @@
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using System.CodeDom.Compiler;
+using System;
+
 
 namespace OpBivak.ios
 {
 	partial class SettingsViewController : UITableViewController
 	{
+		public SettingsViewController (IntPtr handle) : base (handle)
+		{
+			Title = NSBundle.MainBundle.LocalizedString ("Instellingen", "Instellingen");
+			TabBarItem.Image = UIImage.FromBundle ("gear");
+
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad();
+			this.txtVoornaam.ShouldReturn += (textField) => {
+				this.txtLastName.BecomeFirstResponder();
+				return true;
+			};
+
+			this.txtLastName.ShouldReturn += (textField) => {
+				this.txtStamNummer.BecomeFirstResponder();
+				return true;
+			};
+			this.txtStamNummer.ShouldReturn += (textField) => {
+				this.txtGroepNaam.BecomeFirstResponder();
+				return true;
+			};
+			this.txtGroepNaam.ShouldReturn += (textField) => {
+				this.txtGroepGemeente.BecomeFirstResponder();
+				return true;
+			};
+			this.txtGroepGemeente.ShouldReturn += (textField) => {
+				textField.ResignFirstResponder();
+				return true;
+			};
+		
+
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+			// get settings
+
+			string firstname = NSUserDefaults.StandardUserDefaults.StringForKey ("firstNameKey");
+			string lastname = NSUserDefaults.StandardUserDefaults.StringForKey("lastNameKey");
+			string stamnummer = NSUserDefaults.StandardUserDefaults.StringForKey("stamNummerKey");
+			string groep = NSUserDefaults.StandardUserDefaults.StringForKey("chiroGroepKey");
+			string gemeente = NSUserDefaults.StandardUserDefaults.StringForKey("groepGemeenteKey");
+			if (!string.IsNullOrWhiteSpace (firstname)) {
+				txtVoornaam.Text = firstname;
+			}
+			if (!string.IsNullOrWhiteSpace(lastname)){
+					txtLastName.Text= lastname;
+			}
+			if (!string.IsNullOrWhiteSpace(stamnummer)){
+				txtStamNummer.Text= stamnummer;
+			}
+			if (!string.IsNullOrWhiteSpace(groep)){
+				txtGroepNaam.Text= groep;
+			}
+			if (!string.IsNullOrWhiteSpace(gemeente)){
+				txtGroepGemeente.Text= gemeente;
+			}
+			//btnSendSms.SetState (NSUserDefaults.StandardUserDefaults.BoolForKey ("noSmsKey"), false);
+		}
+
+		partial void txtName_Changed (UITextField sender){
+			saveSettings();
+		}
+
+		public void saveSettings(){
+			NSUserDefaults.StandardUserDefaults.SetString(txtVoornaam.Text, "firstNameKey");
+			NSUserDefaults.StandardUserDefaults.SetString(txtLastName.Text, "lastNameKey");
+			NSUserDefaults.StandardUserDefaults.SetString(txtStamNummer.Text, "stamNummerKey");
+			NSUserDefaults.StandardUserDefaults.SetString(txtGroepNaam.Text, "chiroGroepKey");
+			NSUserDefaults.StandardUserDefaults.SetString(txtGroepGemeente.Text, "groepGemeenteKey");
+			//if btnSendSms.
+			//NSUserDefaults.StandardUserDefaults.SetValueForKey((bool)btnSendSms.State, "noSmsKey");
+		}
 	}
 }
--- OpBivak.ios/SettingsViewController.designer.cs
+++ OpBivak.ios/SettingsViewController.designer.cs
@@ -13,8 +13,52 @@ namespace OpBivak.ios
 	[Register ("SettingsViewController")]
 	partial class SettingsViewController
 	{
+		[Outlet]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UITextField txtGroepGemeente { get; set; }
+
+		[Outlet]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UITextField txtGroepNaam { get; set; }
+
+		[Outlet]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UITextField txtLastName { get; set; }
+
+		[Outlet]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UITextField txtStamNummer { get; set; }
+
+		[Outlet]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UITextField txtVoornaam { get; set; }
+
+		[Action ("txtName_Changed:")]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		partial void txtName_Changed (UITextField sender);
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (txtGroepGemeente != null) {
+				txtGroepGemeente.Dispose ();
+				txtGroepGemeente = null;
+			}
+			if (txtGroepNaam != null) {
+				txtGroepNaam.Dispose ();
+				txtGroepNaam = null;
+			}
+			if (txtLastName != null) {
+				txtLastName.Dispose ();
+				txtLastName = null;
+			}
+			if (txtStamNummer != null) {
+				txtStamNummer.Dispose ();
+				txtStamNummer = null;
+			}
+			if (txtVoornaam != null) {
+				txtVoornaam.Dispose ();
+				txtVoornaam = null;
+			}
 		}
 	}
 }
